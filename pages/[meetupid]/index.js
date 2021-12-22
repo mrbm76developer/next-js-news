5d881211fa2f34b11481b62258e1e2ad8e2d8a9e
Mohammad Reza <mohammadrezabehzadimoqadam@gmail.com>
2021-12-22 14:45:31 +0330

    add gitignore

--- pages/[meetupid]/index.js
+++ pages/[meetupid]/index.js
@@ -1,6 +1,16 @@
-
+import { Fragment } from "react";
 
 const MeetupDetails = () => {
-
-}
+  return (
+    <Fragment>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+        alt="A First Meetup"
+      />
+      <h1>A First Meetup</h1>
+      <address>Some street 5,Some City</address>
+      <p>The meetup description</p>
+    </Fragment>
+  );
+};
 export default MeetupDetails;
